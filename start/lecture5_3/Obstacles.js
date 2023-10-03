1eda50cd9725e595be519adb687b6c69b0adfc55
Braian-92 <zamudiobraianhernan@gmail.com>
2023-10-02 22:30:45 -0300

    indentado obstaculos FIX

--- start/lecture5_3/Obstacles.js
+++ start/lecture5_3/Obstacles.js
@@ -1,99 +1,106 @@
-import { Group, Vector3 } from '../../libs/three137/three.module.js';
-import { GLTFLoader } from '../../libs/three137/GLTFLoader.js';
+import {
+  Group,
+  Vector3
+} from '../../libs/three137/three.module.js';
+import {
+  GLTFLoader
+} from '../../libs/three137/GLTFLoader.js';
 
-class Obstacles{
-    constructor(game){
-        this.assetsPath = game.assetsPath;
-        this.loadingBar = game.loadingBar;
-		this.game = game;
-		this.scene = game.scene;
-        this.loadStar();
-		this.loadBomb();
-		this.tmpPos = new Vector3();
-    }
+class Obstacles {
+  constructor(game) {
+    this.assetsPath = game.assetsPath;
+    this.loadingBar = game.loadingBar;
+    this.game = game;
+    this.scene = game.scene;
+    this.loadStar();
+    this.loadBomb();
+    this.tmpPos = new Vector3();
+  }
 
-    loadStar(){
-    	const loader = new GLTFLoader( ).setPath(`${this.assetsPath}plane/`);
-        this.ready = false;
-        
-		// Load a glTF resource
-		loader.load(
-			// resource URL
-			'star.glb',
-			// called when the resource is loaded
-			gltf => {
+  loadStar() {
+    const loader = new GLTFLoader().setPath(`${this.assetsPath}plane/`);
+    this.ready = false;
 
-                this.star = gltf.scene.children[0];
+    // Load a glTF resource
+    loader.load(
+      // resource URL
+      'star.glb',
+      // called when the resource is loaded
+      gltf => {
 
-                this.star.name = 'star';
+        this.star = gltf.scene.children[0];
 
-				if (this.bomb !== undefined) this.initialize();
+        this.star.name = 'star';
 
-			},
-			// called while loading is progressing
-			xhr => {
+        if (this.bomb !== undefined) this.initialize();
 
-                this.loadingBar.update('star', xhr.loaded, xhr.total );
-			
-			},
-			// called when loading has errors
-			err => {
+      },
+      // called while loading is progressing
+      xhr => {
 
-				console.error( err );
+        this.loadingBar.update('star', xhr.loaded, xhr.total);
 
-			}
-		);
-	}	
+      },
+      // called when loading has errors
+      err => {
 
-    loadBomb(){
-    	const loader = new GLTFLoader( ).setPath(`${this.assetsPath}plane/`);
-        
-		// Load a glTF resource
-		loader.load(
-			// resource URL
-			'bomb.glb',
-			// called when the resource is loaded
-			gltf => {
+        console.error(err);
 
-                this.bomb = gltf.scene.children[0];
+      }
+    );
+  }
 
-                if (this.star !== undefined) this.initialize();
+  loadBomb() {
+    const loader = new GLTFLoader().setPath(`${this.assetsPath}plane/`);
 
-			},
-			// called while loading is progressing
-			xhr => {
+    // Load a glTF resource
+    loader.load(
+      // resource URL
+      'bomb.glb',
+      // called when the resource is loaded
+      gltf => {
 
-				this.loadingBar.update('bomb', xhr.loaded, xhr.total );
-				
-			},
-			// called when loading has errors
-			err => {
+        this.bomb = gltf.scene.children[0];
 
-				console.error( err );
+        if (this.star !== undefined) this.initialize();
 
-			}
-		);
-	}
+      },
+      // called while loading is progressing
+      xhr => {
 
-	initialize(){
-        
-    }
+        this.loadingBar.update('bomb', xhr.loaded, xhr.total);
 
-    reset(){
-        
-    }
+      },
+      // called when loading has errors
+      err => {
 
-    respawnObstacle( obstacle ){
-        
-    }
+        console.error(err);
 
-	update(pos){
-        
-    }
+      }
+    );
+  }
 
-	hit(obj){
-		
-	}
+  initialize() {
+
+  }
+
+  reset() {
+
+  }
+
+  respawnObstacle(obstacle) {
+
+  }
+
+  update(pos) {
+
+  }
+
+  hit(obj) {
+
+  }
 }
 
-export { Obstacles };
+export {
+  Obstacles
+};
