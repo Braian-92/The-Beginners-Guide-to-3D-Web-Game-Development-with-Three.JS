c59a480a1cdb94325a75463e59c9f79aa8da67bb
Braian-92 <zamudiobraianhernan@gmail.com>
2023-10-11 01:12:38 -0300

    base indentado juego de billar (2)

--- start/lecture7_2/Table.js
+++ start/lecture7_2/Table.js
@@ -1,16 +1,18 @@
 import * as CANNON from '../../libs/cannon-es.js';
 
-class Table{
+class Table {
 
-    static LENGTH = 2.7432;
-    static WIDTH = 1.3716;
-    static HEIGHT = 0.2;
-    static FLOOR_MATERIAL = new CANNON.Material('floorMaterial');
- 
-    constructor(game){
-        
-    }
+  static LENGTH = 2.7432;
+  static WIDTH = 1.3716;
+  static HEIGHT = 0.2;
+  static FLOOR_MATERIAL = new CANNON.Material('floorMaterial');
+
+  constructor(game) {
+
+  }
 
 }
 
-export { Table };
+export {
+  Table
+};
